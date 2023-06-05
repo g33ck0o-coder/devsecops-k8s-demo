74755276c70be004617a9f0e876612144cadeacb
g33ck0o-coder <79891810+g33ck0o-coder@users.noreply.github.com>
2023-06-05 15:33:13 -0600

    trivyignore test

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -3,7 +3,9 @@
 echo $imageName #getting Image Name from env variable
 
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 1 --ignore-unfixed --severity CRITICAL --ignorefile .trivyignore --light $imageName
+#docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 1 --ignore-unfixed --severity CRITICAL --ignorefile .trivyignore --light $imageName
+
+docker run --rm -v "$PWD/.trivyignore":/.trityignore:ro -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 1 --ignore-unfixed --sre-unfixed --severity CRITICAL --ignorefile /.trivyignore --light $imageName
 
 #Trivy scan result processing
 exit_code=$?
