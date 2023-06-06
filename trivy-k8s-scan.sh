cda18ca62f669cd7bd3465e99868f175675a2be9
g33ck0o-coder <79891810+g33ck0o-coder@users.noreply.github.com>
2023-06-05 23:03:37 -0600

    fix all false positives

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -4,8 +4,7 @@ echo $imageName #getting Image Name from env variable
 
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
 #docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 1 --ignore-unfixed --severity CRITICAL --ignorefile .trivyignore --light $imageName
-
-docker run --rm -v "$PWD/.trivyignore":/.trityignore:ro -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.41.0 -q image --exit-code 1 --ignore-unfixed --severity CRITICAL --ignorefile /.trivyignore --light $imageName
+docker run --rm -v -v $PWD/.trivyignore:/.trivyignore -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --ignore-unfixed --severity CRITICAL --light $imageName
 
 #Trivy scan result processing
 exit_code=$?
